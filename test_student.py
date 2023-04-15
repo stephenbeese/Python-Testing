2bb345d204afed7176bb2095b36ddd49f69d74ca
stephenbeese <stephenbeese1@live.co.uk>
2023-04-15 11:21:55 +0000

    Add email test

---
+++ test_student.py
@@ -0,0 +1,25 @@
+import unittest
+from student import Student
+
+
+class TestStudent(unittest.TestCase):
+    def test_full_name(self):
+        student = Student('John', 'Doe')
+
+        self.assertEqual(student.full_name, 'John Doe')
+
+    def test_alert_santa(self):
+        student = Student('John', 'Doe')
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list)
+
+    def test_email_address(self):
+        student = Student('John', 'Doe')
+        # student.email_address()
+
+        self.assertEqual(student.email_address, 'john.doe@email.com')
+
+
+if __name__ == '__main__':
+    unittest.main()
